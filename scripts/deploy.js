75bdc98d418a4bb5ec67eb490a6719a2420eed3b
Grant Smith <grantdotlocal@gmail.com>
2019-09-17 23:03:36 -0500

    fact correction, better mobile style

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ const { exec } = require('child_process');
 const path = require('path')
 require('dotenv').config({path: path.resolve(__dirname, '../.env') });
 
-exec(`rsync -avHz dist/ -e ssh ${process.env.RSYNC_CONNECTION}`, (error, stdout, stderr) => {
+exec(`rsync -avHzv dist/ -e ssh ${process.env.RSYNC_CONNECTION}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`); 
       return;
